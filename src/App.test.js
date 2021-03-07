9bcdcf88b02f88f571837e02efce827f3a532fb4
Murugesh Aravind <murugesh@amazecodes.com>
2021-03-07 15:00:22 +0530

    changes made for testing the app in jest

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
-import { render, screen } from '@testing-library/react';
+import { mount } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Counter Testing', () => {
+  let wrapper;
+  beforeAll(() => {
+    wrapper = mount(<App />);
+  });
+  test('renders title of counter', () => {
+    expect(wrapper.find('h1').text()).toEqual('This is counter app');
+  });
+  test('render a button with the text of `+` for increment', () => {
+    expect(wrapper.find('#increment-btn').text()).toBe('+');
+  });
+  test('render a button with the text of `-` for decrement', () => {
+    expect(wrapper.find('#decrement-btn').text()).toBe('-');
+  });
+  test('render the initial value of state in a div', () => {
+    expect(wrapper.find('#counter-value').text()).toBe('0');
+  });
+  test('increment the value on each click of increment button', () => {
+    wrapper.find('#increment-btn').simulate('click');
+    expect(wrapper.find('#counter-value').text()).toBe('1');
+  });
+  test('do not go beyond the value 0 on click of decrement button', () => {
+    wrapper.find('#decrement-btn').simulate('click');
+    expect(wrapper.find('#counter-value').text()).toBe('0');
+  });
+  test('check for the button click', () => {
+    wrapper.find('#fetch-comment').simulate('click');
+    expect(wrapper.fetchComment).toBeCalled();
+  });
 });
---
+++ src/Counter.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import axios from 'axios';
+
+function Counter() {
+  const [counter, setCounter] = React.useState(0);
+  const [comment, setComment] = React.useState('');
+  const checkDecrementCount = () => {
+    if (counter > 0) return setCounter(counter - 1);
+  };
+  const fetchComment = async () => {
+    let response = await axios.get(
+      'https://jsonplaceholder.typicode.com/comments/1',
+    );
+    setComment(response.data);
+  };
+  return (
+    <div>
+      <h1>This is counter app</h1>
+      <div id="counter-value">{counter}</div>
+      <button id="increment-btn" onClick={() => setCounter(counter + 1)}>
+        {'+'}
+      </button>
+      <button id="decrement-btn" onClick={() => checkDecrementCount()}>
+        {'-'}
+      </button>
+      <button id="fetch-comment" onClick={() => fetchComment()}>
+        {'Fetch Comment'}
+      </button>
+      <pre>{comment ? comment : ''}</pre>
+    </div>
+  );
+}
+
+export default Counter;
